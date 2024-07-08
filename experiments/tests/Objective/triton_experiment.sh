2ba62268f782a4cc0464fae9098003b64aff2db2
DimitrisGkoutzounis <DimitrisGkoutzounis>
2024-07-08 13:55:57 +0300

    updated on folder

---
+++ experiments/tests/Objective/triton_experiment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=experiment
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=02:30:00
+
+
+
+source activate conda-example  # Activate your virtual environment
+
+srun python3 experiment_objective.py
+
